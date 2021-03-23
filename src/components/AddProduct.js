f542f65f12af98fdac2250c8c55e02adf961d08c
DavidLabaz <devdlab@gmail.com>
2021-03-23 09:41:22 +0200

    final -v

---
+++ src/components/AddProduct.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+export default class  extends React.Component {
+
+    state = {
+        name: '',
+        description: '',
+        count: ''
+    }
+
+    nameChange = (e) => {
+        this.setState({name: e.target.value});
+    }
+    descriptionChange = (e) => {
+        this.setState({description: e.target.value});
+    }
+    countChange = (e) => {
+        this.setState({count: e.target.value});
+    }
+    addPost = () => {
+        this.props.addPost(this.state)
+        this.props.closePopup()
+    }
+    closePopup = () => {
+        this.props.closePopup()
+    }
+
+    render() {
+        return (
+
+            <div className="modal">
+
+                <div className="modal-content">
+                    <h1>{this.props.data ? '' : 'Add Product'}</h1>
+                    <div className='input'>
+                        <label>Name</label>
+                        <input onChange={this.nameChange} value={this.state.name} type="text"/>
+                    </div>
+                    <div className='input'>
+                        <label>Description</label>
+                        <textarea wrap='hard' rows='5' cols='40' value={this.state.description}
+                                  onChange={this.descriptionChange}/>
+                    </div>
+                    <div className='input'>
+                        <label>Count</label>
+                        <input onChange={this.countChange} value={this.state.count} type="number"/>
+                    </div>
+                    <footer>
+                        <button onClick={this.closePopup}>Cancel</button>
+                        <button onClick={this.addPost}>Save</button>
+                    </footer>
+                </div>
+
+            </div>
+        )
+    }
+}
---
+++ src/components/Details.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+export default class Product extends React.Component {
+
+    state = {
+        loading: true,
+        product: null,
+        products: null,
+        openPopup: false
+    }
+
+    async componentDidMount() {
+        const id = this.props.match.params.id;
+        const url = `https://6057ae5fc3f49200173ace62.mockapi.io/api/v1/product/${id}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({product: data, loading: false})
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                {this.state.loading || !this.state.product ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div className='product-list'>
+                        <div className='product'>
+                            <img src={this.state.product.imageUrl} className='prod-img'  alt='image'/>
+                            <span className='prod-name'>{this.state.product.name}</span>
+                            <span className='prod-desc'>{this.state.product.description}</span>
+                            <span className='prod-color'>color: {this.state.product.color}</span>
+                            <span className='prod-size'>color: {this.state.product.color}</span>
+                            <span className='prod-weight'>weight: {this.state.product.weight}g</span>
+                            <span className='prod-count'>avaible: {this.state.product.count} units</span>
+                            <span className='prod-comments'>comments: <br/><br/> {this.state.product.comments} </span>
+                        </div>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
